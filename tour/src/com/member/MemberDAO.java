4a68cd839f292c9f3c49b9581ce66b080c84e4ea
sist <sist@DESKTOP-7151BDS>
2019-03-05 15:29:29 +0900

    readMember() 메소드 생성

--- tour/src/com/member/MemberDAO.java
+++ tour/src/com/member/MemberDAO.java
@@ -2,6 +2,7 @@ package com.member;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.Util.DBConn;
@@ -34,7 +35,97 @@ public class MemberDAO {
 				}
 			}
 		}
+		return result;
+	}
+	
+	public MemberDTO readMember(String userId) {
+		MemberDTO dto=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		String sql;
 		
+		try {
+			sql = "SELECT userId, userName, userPwd, tel, created_date, modify_date, userRoll"
+					+ "FROM member WHERE userId = ?";
+			
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, userId);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				dto = new MemberDTO();
+				dto.setUserId(rs.getString("userId"));
+				dto.setUserName(rs.getString("userId"));
+				dto.setUserPwd(rs.getString("userId"));
+				dto.setTel(rs.getString("userId"));
+				dto.setCreated_date(rs.getString("userId"));
+				dto.setModify_date(rs.getString("userId"));
+				dto.setUserRoll(rs.getInt("userId"));				
+			}
+			
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			if(rs!=null) {
+				try {
+					rs.close();
+				} catch (Exception e2) {
+				}
+			}
+			if(pstmt!=null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
+		
+		return dto;
+	}
+	
+	public int updateMember() {
+		int result =0;
+		PreparedStatement pstmt=null;
+		String sql;
+		
+		try {
+			sql = "";
+			pstmt=conn.prepareStatement(sql);
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			if(pstmt!=null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
+		
+		return result;
+	}
+	
+	public int deleteMember() {
+		int result=0;
+		PreparedStatement pstmt=null;
+		String sql;
+		
+		try {
+			sql = "";
+			pstmt=conn.prepareStatement(sql);
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			if(pstmt!=null) {
+				try {
+					pstmt.close();
+				} catch (Exception e2) {
+				}
+			}
+		}
 		
 		return result;
 	}
