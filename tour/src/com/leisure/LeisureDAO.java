04aed55ac2076a30090be7a7fc87f234c9bd8a92
sist <sist@DESKTOP-PPJ4R3Q>
2019-03-05 18:54:00 +0900

    dao

---
+++ tour/src/com/leisure/LeisureDAO.java
@@ -0,0 +1,5 @@
+package com.leisure;
+
+public class LeisureDAO {
+
+}
---
+++ tour/src/com/leisure/LeisureDTO.java
@@ -0,0 +1,103 @@
+package com.leisure;
+
+public class LeisureDTO {
+	private int num;
+	private String userId;
+	private String subject;
+	private String opening;
+	private String useTime;
+	private String tel;
+	private String address;
+	private int longitude;
+	private int latitude;
+	private String introduction;
+	private String content;
+	private int hitCount;
+	private String created;
+	private String imageFileName;
+		
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getOpening() {
+		return opening;
+	}
+	public void setOpening(String opening) {
+		this.opening = opening;
+	}
+	public String getUseTime() {
+		return useTime;
+	}
+	public void setUseTime(String useTime) {
+		this.useTime = useTime;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(int longitude) {
+		this.longitude = longitude;
+	}
+	public int getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(int latitude) {
+		this.latitude = latitude;
+	}
+	public String getIntroduction() {
+		return introduction;
+	}
+	public void setIntroduction(String introduction) {
+		this.introduction = introduction;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getHitCount() {
+		return hitCount;
+	}
+	public void setHitCount(int hitCount) {
+		this.hitCount = hitCount;
+	}
+	public String getCreated() {
+		return created;
+	}
+	public void setCreated(String created) {
+		this.created = created;
+	}
+	public String getImageFileName() {
+		return imageFileName;
+	}
+	public void setImageFileName(String imageFileName) {
+		this.imageFileName = imageFileName;
+	}	
+}
