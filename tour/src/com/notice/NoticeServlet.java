046249e854a260a86940ee996f08805ec64991a6
sist <sist@DESKTOP-V9VOR7F>
2019-03-15 11:59:06 +0900

    import-수정

--- tour/src/com/notice/NoticeServlet.java
+++ tour/src/com/notice/NoticeServlet.java
@@ -15,7 +15,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.websocket.Session;
 
 import com.Util.FileManager;
 import com.Util.MyServlet;
